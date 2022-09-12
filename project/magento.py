0a8fba07056cff25424ab16107ec96379e0fc06a
removedporn <86824510+removedporn@users.noreply.github.com>
2022-09-12 23:01:20 +0800

    Delete project directory

--- project/magento.py
+++
@@ -1,6 +0,0 @@
-from .remote import RemoteProject
-
-
-class Magento2ce(RemoteProject):
-    major_version = '2.3'
-    remote = 'https://github.com/magento/magento2.git'
--- project/sculpin.py
+++
@@ -1,6 +0,0 @@
-from .remote import RemoteProject
-
-
-class Sculpin(RemoteProject):
-    major_version = '3'
-    remote = 'https://github.com/sculpin/sculpin-blog-skeleton.git'
