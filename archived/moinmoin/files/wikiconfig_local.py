2c7061478d4239eba14c019525a6343635c7eadd
removedporn <86824510+removedporn@users.noreply.github.com>
2022-09-12 23:00:59 +0800

    Delete archived directory

--- archived/moinmoin/files/wikiconfig_local.py
+++
@@ -1,7 +0,0 @@
-from wikiconfig import LocalConfig
-
-
-class Config(LocalConfig):
-    sitename = u'MoinMoin on Platform.sh'
-    # Uncomment this to change 'LanguageSetup' default front page and start editing
-    # page_front_page = u'FrontPage'
